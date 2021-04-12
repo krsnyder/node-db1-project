6a8262975cba67cfa8200edc5e745b8aba0953c1
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-12 13:35:30 -0400

    Build checkAccountId middleware function

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
 }
@@ -6,6 +8,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const checkId = await Accounts.getById(req.params.id)
+    if (checkId) {
+      req.post = post
+    } else {
+      res.status(404).json({
+        message: "Account not found"
+      })
+    }
+  } catch(err) {
+    next(err)
+  }
 }
